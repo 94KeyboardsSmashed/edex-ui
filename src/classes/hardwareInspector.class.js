822fae3d5e2abd6ef96595b0708df1657da0da3b
94KeyboardsSmashed <you@example.com>
2020-02-23 14:21:28 -0600

    added hardwareInspector JSON connection

--- src/classes/hardwareInspector.class.js
+++ src/classes/hardwareInspector.class.js
@@ -8,16 +8,16 @@ class HardwareInspector {
         this._element.setAttribute("id", "mod_hardwareInspector");
         this._element.innerHTML = `<div id="mod_hardwareInspector_inner">
             <div>
-                <h1>MANUFACTURER</h1>
-                <h2 id="mod_hardwareInspector_manufacturer" >NONE</h2>
+                <h1>HARDWARE</h1>
+                <h2 id="mod_hardwareInspector_hardware" >NONE</h2>
             </div>
             <div>
-                <h1>MODEL</h1>
-                <h2 id="mod_hardwareInspector_model" >NONE</h2>
+                <h1>TYPE</h1>
+                <h2 id="mod_hardwareInspector_type" >NONE</h2>
             </div>
             <div>
-                <h1>CHASSIS</h1>
-                <h2 id="mod_hardwareInspector_chassis" >NONE</h2>
+                <h1>VERSION</h1>
+                <h2 id="mod_hardwareInspector_version" >NONE</h2>
             </div>
         </div>`;
 
@@ -29,20 +29,9 @@ class HardwareInspector {
         }, 20000);
     }
     updateInfo() {
-        window.si.system().then(d => {
-            window.si.chassis().then(e => {
-                document.getElementById("mod_hardwareInspector_manufacturer").innerText = this._trimDataString(d.manufacturer);
-                document.getElementById("mod_hardwareInspector_model").innerText = this._trimDataString(d.model, d.manufacturer, e.type);
-                document.getElementById("mod_hardwareInspector_chassis").innerText = e.type;
-            });
-        });
-    }
-    _trimDataString(str, ...filters) {
-        return str.trim().split(" ").filter(word => {
-            if (typeof filters !== "object") return true;
-
-            return !filters.includes(word);
-        }).slice(0, 2).join(" ");
+        document.getElementById("mod_hardwareInspector_hardware").innerText = window.jsondata.hardwareInspector.hardware;
+        document.getElementById("mod_hardwareInspector_type").innerText = window.jsondata.hardwareInspector.type;
+        document.getElementById("mod_hardwareInspector_version").innerText = window.jsondata.hardwareInspector.version;
     }
 }
 
